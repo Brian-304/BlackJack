68116bd8a93506fd8e159cb776b64a64c421a4c0
Brian Tabios <105179017+Brian-304@users.noreply.github.com>
2024-12-07 21:19:40 -0800

    Initial commit
    Created player class

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
+#include "Player.h"
 
 using namespace std;
 
 int main() {
-	cout << "Hello World";
+	string player1, player2;
+	
+	cout << "Enter name for Player 1: ";
+	getline(cin, player1);
+	cout << "Enter naem for player 2: ";
+	getline(cin, player2);
+
+	Player p1(player1);
+	Player p2(player2);
+
+	cout << p1.getName() << "\n";
+	cout << p2.getName() << "\n";
 	
 
 	return 0;
---
+++ Player.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Player
+{
+private:
+    string name;
+public:
+    Player(const string& playerName) : name(playerName) {}
+
+    string getName() const {
+        return name;
+    }
+};
